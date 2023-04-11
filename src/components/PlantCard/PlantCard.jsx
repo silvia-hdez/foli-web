185a1ea93bed419c45a8a77b066b1c9652d25d58
silvi-hdez <silviahdez.b@gmail.com>
2023-04-11 12:28:30 +0200

    no me sale eliminar bookmark

--- src/components/PlantCard/PlantCard.jsx
+++ src/components/PlantCard/PlantCard.jsx
@@ -1,17 +1,41 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./PlantCard.css";
 import { Link } from "react-router-dom";
-import { postSavePlant } from "../../services/SaveService";
+import { deleteSavePlant, getSavePlants, postSavePlant } from "../../services/SaveService";
 
 const PlantCard = ({ plant }) => {
   const [isBookmarked, setIsBookmarked] = useState(false);
+  const [savedPlants, setSavedPlants] = useState([])
+
+  useEffect(() => {
+    getSavePlants()
+      .then((plants)=>setSavedPlants(plants))
+      .catch((err)=>console.log(err))
+  }, [])
 
   const handleBookmark = () => {
     setIsBookmarked(!isBookmarked);
+    const isPlantSaved = savedPlants.some((savedPlant) => savedPlant.plant._id === plant._id);
+
+    if(isPlantSaved) {
+      const savedPlant = savedPlants.find((savedPlant) => savedPlant.plant._id === plant._id);
+      deleteSavePlant(savedPlant._id)
+      .then((response) => {
+        console.log(response.data);
+        setSavedPlants(savedPlants.filter((savedPlant) => savedPlant.plant._id !== plant._id));
+      })
+      .catch((err) => console.log(err));
+    }
     postSavePlant(plant._id)
-      .then((response) => console.log(response))
+      .then((save) => {
+      console.log(save)
+      setSavedPlants([...savedPlants, save]);
+      })
+    
       .catch((err)=>console.log(err))
   };
+
+
 
   return (
     <div className="plantCard">
--- src/services/SaveService.js
+++ src/services/SaveService.js
@@ -5,3 +5,5 @@ const authenticatedHttp = createHttp(true)
 export const postSavePlant = (plantId) =>
   authenticatedHttp.post(`/plants/save/${plantId}`);
 export const getSavePlants = () => authenticatedHttp.get("/save-plants");
+
+export const deleteSavePlant = (saveId) => authenticatedHttp.post(`/saves/${saveId}/delete`)
