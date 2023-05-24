c25752f3b2b15759c38156f23b0f378635b36942
silvi-hdez <silviahdez.b@gmail.com>
2023-05-24 22:40:52 +0200

    improve form logs

--- src/views/SignUp/SignUp.jsx
+++ src/views/SignUp/SignUp.jsx
@@ -1,119 +1,167 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import { signup as signupService } from "../../services/AuthService";
 import { useFormik } from "formik";
-import { signupSchema } from '../../schemas/signup.schema';
-import FormControl from '../../components/misc/FormControl/FormControl';
-import Input from '../../components/misc/Input/Input';
-import { Link, Navigate } from 'react-router-dom';
-
-
+import { signupSchema } from "../../schemas/signup.schema";
+import FormControl from "../../components/misc/FormControl/FormControl";
+import Input from "../../components/misc/Input/Input";
+import { Link, Navigate } from "react-router-dom";
 
 const initialValues = {
-    fullName: "",
-    userName: "",
-    userPhone: "",
-    email: "",
-    password: ""
-}
+  fullName: "",
+  userName: "",
+  userPhone: "",
+  email: "",
+  password: "",
+};
 
 const SignUp = () => {
-    let [signedUp, setSignedUp] = useState(false)
-    const {
-        values,
-        errors,
-        touched,
-        handleChange,
-        handleBlur,
-        isSubmitting,
-        handleSubmit,
-        setSubmitting,
-        setFieldError
-      } = useFormik({
-        initialValues: initialValues,
-        validateOnBlur: true,
-        validateOnChange: false,
-        validationSchema: signupSchema,
-        onSubmit: (values) => {
-          signupService({ email: values.email, password: values.password, fullName: values.fullName,
-                        userName: values.userName, userPhone: values.userPhone }) 
-            .then((response) => {
-            //   console.log(JSON.stringify(response))
-              setSignedUp(true)
-            })
-            .catch((err) => {
-                if (err?.response?.data?.message) {
-                    setFieldError("email", err?.response?.data?.message);
-                  } else {
-                    setFieldError("email", err.message);
-                  }
-                setSubmitting(false);
-            });
-        },
-      });
-    
+  let [signedUp, setSignedUp] = useState(false);
+  const {
+    values,
+    errors,
+    touched,
+    handleChange,
+    handleBlur,
+    isSubmitting,
+    handleSubmit,
+    setSubmitting,
+    setFieldError,
+  } = useFormik({
+    initialValues: initialValues,
+    validateOnBlur: true,
+    validateOnChange: false,
+    validationSchema: signupSchema,
+    onSubmit: (values) => {
+      signupService({
+        email: values.email,
+        password: values.password,
+        fullName: values.fullName,
+        userName: values.userName,
+        userPhone: values.userPhone,
+      })
+        .then((response) => {
+          //   console.log(JSON.stringify(response))
+          setSignedUp(true);
+        })
+        .catch((err) => {
+          console.log(err);
+          console.log(err.status);
+          if (err?.response?.data && err.response.status === 409) {
+            const keyErr = Object.keys(err.response.data)[0];
+            setFieldError(keyErr, `The ${keyErr} already exists`);
+          } else {
+            setFieldError("email", err.message);
+          }
+          setSubmitting(false);
+        });
+    },
+  });
 
-    return (
-        <div className="Login">
+  return (
+    <div className="Login">
+      <div className="BoxLogin">
+        <h1>Create your account</h1>
 
-        <div className="BoxLogin">
+        <form onSubmit={handleSubmit}>
+          <FormControl
+            text="Full Name"
+            error={touched.fullName && errors.fullName}
+            htmlFor="fullName"
+          >
+            <Input
+              id="fullName"
+              name="fullName"
+              onBlur={handleBlur}
+              onChange={handleChange}
+              value={values.fullName}
+              error={touched.fullName && errors.fullName}
+              placeholder="Full Name"
+            />
+          </FormControl>
 
-        
-            <h1>Create your account</h1>
+          <FormControl
+            text="User Name"
+            error={touched.userName && errors.userName}
+            htmlFor="userName"
+          >
+            <Input
+              id="userName"
+              name="userName"
+              onBlur={handleBlur}
+              onChange={handleChange}
+              value={values.userName}
+              error={touched.userName && errors.userName}
+              placeholder="User Name"
+            />
+          </FormControl>
 
-            <form onSubmit={handleSubmit}>
-                <FormControl text= "Full Name" error={touched.fullName && errors.fullName} htmlFor="fullName">
-                    <Input id="fullName" name="fullName" onBlur={handleBlur}
-                        onChange={handleChange} value={values.fullName} error={touched.fullName && errors.fullName}
-                        placeholder="Full Name" 
-                    />
-                </FormControl>
+          <FormControl
+            text="Phone"
+            error={touched.userPhone && errors.userPhone}
+            htmlFor="userPhone"
+          >
+            <Input
+              id="userPhone"
+              name="userPhone"
+              onBlur={handleBlur}
+              onChange={handleChange}
+              value={values.userPhone}
+              error={touched.userPhone && errors.userPhone}
+              placeholder="Phone"
+            />
+          </FormControl>
 
-                <FormControl text= "User Name" error={touched.userName && errors.userName} htmlFor="userName">
-                    <Input id="userName" name="userName" onBlur={handleBlur}
-                        onChange={handleChange} value={values.userName} error={touched.userName && errors.userName}
-                        placeholder="User Name" 
-                    />
-                </FormControl>
+          <FormControl
+            text="Email"
+            error={touched.email && errors.email}
+            htmlFor="email"
+          >
+            <Input
+              id="email"
+              name="email"
+              onBlur={handleBlur}
+              onChange={handleChange}
+              value={values.email}
+              error={touched.email && errors.email}
+              placeholder="Email"
+            />
+          </FormControl>
 
-                <FormControl text= "Phone" error={touched.userPhone && errors.userPhone} htmlFor="userPhone">
-                    <Input id="userPhone" name="userPhone" onBlur={handleBlur}
-                        onChange={handleChange} value={values.userPhone} error={touched.userPhone && errors.userPhone}
-                        placeholder="Phone" 
-                    />
-                </FormControl>
+          <FormControl
+            text="Password"
+            error={touched.password && errors.password}
+            htmlFor="password"
+          >
+            <Input
+              id="password"
+              name="password"
+              onBlur={handleBlur}
+              onChange={handleChange}
+              value={values.password}
+              error={touched.password && errors.password}
+              placeholder="*******"
+              type="password"
+            />
+          </FormControl>
 
-                
-                <FormControl text= "Email" error={touched.email && errors.email} htmlFor="email">
-                    <Input id="email" name="email" onBlur={handleBlur}
-                        onChange={handleChange} value={values.email} error={touched.email && errors.email}
-                        placeholder="Email" 
-                    />
-                </FormControl>
-
-                <FormControl text= "Password" error={touched.password && errors.password} htmlFor="password">
-                    <Input id="password" name="password" onBlur={handleBlur}
-                        onChange={handleChange} value={values.password} error={touched.password && errors.password}
-                        placeholder="*******" type='password'
-                    />
-                </FormControl>
-
-                <button
-                    className="SubmitCreatePost"
-                    type="submit"
-                    disabled={isSubmitting}
-                    >
-                    {isSubmitting ? "Submitting..." : "Submit"}
-            </button>
-
-            </form>
-            {signedUp && <Navigate to="/login" />}
-            <div className="LinkRegistro">
-        <p>Have an account already?</p>
-        <Link className="LinkLogin" to='/login'>Log in</Link>
-      </div>
-      </div>
+          <button
+            className="SubmitCreatePost"
+            type="submit"
+            disabled={isSubmitting}
+          >
+            {isSubmitting ? "Submitting..." : "Submit"}
+          </button>
+        </form>
+        {signedUp && <Navigate to="/login" />}
+        <div className="LinkRegistro">
+          <p>Have an account already?</p>
+          <Link className="LinkLogin" to="/login">
+            Log in
+          </Link>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default SignUp;
